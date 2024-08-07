0ac4379f39c6d32f22812e982ffcadf9a29ae34f
Didik K <didikunila@gmail.com>
2024-08-07 07:54:30 +0700

    rps-template

---
+++ includes/rps-template.php
@@ -0,0 +1,273 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Table Example</title>
+    <style>
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+        th, td {
+            border: 1px solid black;
+            padding: 8px;
+            text-align: center;
+        }
+        th {
+            background-color: #d3e8f0;
+        }
+        .header {
+            text-align: center;
+            font-weight: bold;
+        }
+    </style>
+</head>
+<body>
+    <table>
+        <tr>
+            <td rowspan="2"><img src="https://upload.wikimedia.org/wikipedia/commons/0/02/Logo-unila-2018-300x300.png" alt="Logo" width="100"></td>
+            <td colspan="4" class="header">UNIVERSITAS LAMPUNG<br>FAKULTAS MATEMATIKA DAN ILMU PENGETAHUAN ALAM<br>JURUSAN ILMU KOMPUTER<br>RENCANA PEMBELAJARAN SEMESTER</td>
+            <td rowspan="2">RPS<br>3/001</td>
+        </tr>
+        <tr></tr>
+        <tr>
+            <th>MATA KULIAH (MK)</th>
+            <th>KODE</th>
+            <th>Rumpun MK</th>
+            <th>BOBOT (sks)</th>
+            <th>SEMESTER</th>
+            <th>Tgl Penyusunan</th>
+        </tr>
+        <tr>
+            <td>Pemrograman Berorientasi Objek</td>
+            <td>COM620206</td>
+            <td>Rekayasa Perangkat Lunak</td>
+            <td>T=2<br>P=1</td>
+            <td>3</td>
+            <td>5 Juli 2021</td>
+        </tr>
+        <tr>
+            <th>OTORISASI</th>
+            <td colspan="2">Pengembang RPS</td>
+            <td colspan="2">Koordinator RMK</td>
+            <td>Ketua PRODI</td>
+        </tr>
+        <tr>
+            <td></td>
+            <td colspan="2">Didik Kurniawan, M.T.</td>
+            <td colspan="2">Dwi Sakethi, M.Kom</td>
+            <td>Didik Kurniawan, M.T.</td>
+        </tr>
+    </table>
+    <table>
+        <tr>
+            <th rowspan="2">Capaian Pembelajaran (CP)</th>
+            <th colspan="2">CPL-PRODI yang dibebankan pada MK</th>
+        </tr>
+        <tr>
+            <td>CPL-1</td>
+            <td>Have religious attitudes and tolerance to differences, as well as responsible and contribute to law enforcement, ethics, and norms for social life and environmental preservation.</td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>CPL-7</td>
+            <td>Having a complete understanding and ability to use the basic concepts and principles of algorithms and computer programming in computer and intelligent systems.</td>
+        </tr>
+        <tr>
+            <th rowspan="7">Capaian Pembelajaran Mata Kuliah (CPMK)</th>
+            <th class="sub-header" colspan="2">CPMK merupakan turunan/uraian spesifik dari CPL-PRODI yg berkaitan dengan mata kuliah ini</th>
+        </tr>
+        <tr>
+            <td>CPL ⇒ Sub-CPMK</td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>CPL-1</td>
+            <td>Mahasiswa bersikap jujur dalam mengerjakan soal tugas dan ujian</td>
+        </tr>
+        <tr>
+            <td rowspan="4">CPL-7</td>
+            <td>Mampu mengkompilasi Program</td>
+        </tr>
+        <tr>
+            <td>Mampu mengerokesi kesalahan kode program.</td>
+        </tr>
+        <tr>
+            <td>Mampu menghasilkan program bebas yang bebas dari kesalahan sintaks</td>
+        </tr>
+        <tr>
+            <td>Mampu Membuat program berorientasi objek dengan memanfaatkan class<br>Mampu menerjemahkan diagram class ke dalam kode program</td>
+        </tr>
+
+        <tr>
+            <th>Deskripsi Singkat MK</th>
+            <td colspan="2">Mata Kuliah ini mempelajari konsep dan bagaimana implementasi pemrograman berorientasi objek. Pada akhir course peserta diharapkan dapat membuat sebuah sistem atau penyelesaian masalah dengan pemrograman berorientasi objek.</td>
+        </tr>
+        <tr>
+            <th>Bahan Kajian / Materi Pembelajaran</th>
+            <td colspan="2">Materi untuk course ini meliputi: (1) Dasar-dasar pemrograman: tipe data, variabel, array dan struktur kontrol; (2) Konsep OOP: Class, Enkapsulasi, Konstruktor, Overloading, Override, Inheritance, Polymorphism, Abstraksi, dan Interface. (3) Implementasi: Class Diagram, Basis Data dan JavaFX.</td>
+        </tr>
+        <tr>
+            <th>Pustaka</th>
+            <td colspan="2">
+                <strong>Utama :</strong>
+                <ol>
+                    <li>Oracle Academy Java Fundamental Resources (2023)</li>
+                    <li>Bachtiar, A. M. (2023). Pemrograman berorientasi objek menggunakan Java. Jakarta: PT Elex Media Komputindo</li>
+                    <li>Larman, C. (2005). Applying UML and patterns: An introduction to object-oriented analysis and design and iterative development (3rd ed.). Upper Saddle River, NJ: Pearson Education</li>
+                    <li>Aslanyan, V. (2023). Learn Java fundamentals – Object-oriented programming [Full book]</li>
+                    <li>McLaughlin, B. D., Pollice, G., & West, D. (2004). Head first object-oriented analysis and design. Sebastopol, CA: O’Reilly Media</li>
+                </ol>
+                <strong>Pendukung :</strong>
+                <p>Tuliskan pustaka pendukung jika ada, sebagai pengayaan literasi</p>
+            </td>
+        </tr>
+        <tr>
+            <th>Dosen Pengampu</th>
+            <td colspan="2">Didik Kurniawan, S.Si., M.T.</td>
+        </tr>
+        <tr>
+            <th>Matakuliah syarat</th>
+            <td colspan="2">Tidak ada</td>
+        </tr>
+    </table>
+    <table>
+        <tr>
+            <th rowspan="2">Mg Ke-</th>
+            <th rowspan="2">Sub-CPMK<br>(Kemampuan akhir tiap tahapan belajar)</th>
+            <th colspan="2">Penilaian</th>
+            <th colspan="2">Bentuk Pembelajaran, Metode Pembelajaran, Penguasaan Mahasiswa, <br>[Estimasi Waktu]</th>
+            <th rowspan="2">Materi Pembelajaran<br>[Pustaka]</th>
+            <th rowspan="2">Bobot Penilaian (%)</th>
+        </tr>
+        <tr>
+            <th>Indikator</th>
+            <th>Kriteria & Bentuk</th>
+            <th>Luring (offline)</th>
+            <th>Daring (online)</th>
+        </tr>
+        <tr>
+            <td class="center">1</td>
+            <td>Mampu memahami dan menjelaskan teknologi java dan ruang lingkup pemrograman dan dapat membuat program java sederhana: fitur-fitur teknologi Java seperti Java Virtual Machine(JVM), garbage collection, dan code security</td>
+            <td>
+                ● Mampu mengerjakan soal kuis<br>
+                ● Mampu membuat program java sederhana
+            </td>
+            <td>Soal Kuis dan Tugas Terstruktur</td>
+            <td>Kuliah dan Praktikum,<br>Collaborative Learning dan Contextual Learning<br>[2 x 50 menit]<br>[1 x 170 menit]</td>
+            <td></td>
+            <td>Pengenalan Java dan Ruang Lingkup Pemrograman</td>
+            <td class="center"></td>
+        </tr>
+        <tr>
+            <td class="center">2</td>
+            <td>Dapat menjelaskan kembali konsep struktur kontrol kondisi dan pengulangan dalam bahasa pemrograman</td>
+            <td>
+                ● Membuat program dengan menggunakan struktur kontrol percabangan: if, switch ... case<br>
+                ● Membuat program dengan menggunakan struktur kontrol
+            </td>
+            <td>Soal Kuis dan Tugas Terstruktur</td>
+            <td>Kuliah dan Praktikum,<br>Collaborative Learning dan Contextual Learning<br>[2 x 50 menit]<br>[1 x 170 menit]</td>
+            <td></td>
+            <td>Struktur Kontrol</td>
+            <td class="center"></td>
+        </tr>
+    </table>
+    <div class="container">
+        <div class="header">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/0/02/Logo-unila-2018-300x300.png" alt="Logo UNILA">
+            <h1>UNIVERSITAS LAMPUNG</h1>
+            <h2>Fakultas Matematika dan Ilmu Pengetahuan Alam<br>Program Studi Ilmu Komputer</h2>
+            <h3>RENCANA TUGAS MAHASISWA</h3>
+        </div>
+
+        <div class="section">
+            <div class="section-content">
+                <table>
+                    <tr>
+                        <td><strong>MATA KULIAH</strong></td>
+                        <td colspan="5">Pemrograman Berorientasi Objek</td>
+                    </tr>
+                    <tr>
+                        <td><strong>KODE</strong></td>
+                        <td>COM620206</td>
+                        <td><strong>sks</strong></td>
+                        <td>3</td>
+                        <td><strong>SEMESTER</strong></td>
+                        <td>3</td>
+                    </tr>
+                    <tr>
+                        <td><strong>DOSEN PENGAMPU</strong></td>
+                        <td colspan="5">Didik Kurniawan</td>
+                    </tr>
+                    <tr>
+                        <td><strong>BENTUK TUGAS</strong></td>
+                        <td>Case Base</td>
+                        <td><strong>WAKTU PENGERJAAN TUGAS</strong></td>
+                        <td colspan="3">1 Minggu</td>
+                    </tr>
+                    <tr>
+                        <td><strong>JUDUL TUGAS</strong></td>
+                        <td colspan="5">Studi Kasus Implementasi Konsep Class dan Enkapsulasi</td>
+                    </tr>
+                    <tr>
+                        <td><strong>SUB CAPAIAN PEMBELAJARAN MATA KULIAH</strong></td>
+                        <td colspan="5">
+                            <ul>
+                                <li>Mampu mengkompilasi Program</li>
+                                <li>Mampu mengerokesi kesalahan kode program.</li>
+                            </ul>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><strong>DISKRIPSI TUGAS</strong></td>
+                        <td colspan="5">
+                            Studi Kasus: Manajemen Sumber Daya Manusia di Perusahaan ABC<br>
+                            Terakhir, proyek-proyek yang dikerjakan oleh karyawan diwakili oleh kelas Project. Mereka memiliki atribut yang mencakup nama proyek dan daftar karyawan.
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><strong>METODE PENGERJAAN TUGAS</strong></td>
+                        <td colspan="5">
+                            1. Mandiri<br>
+                            2. Membuat kode program sesuai dengan konsep Pemrograman Berorientasi Objek dengan Java
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><strong>BENTUK DAN FORMAT LUARAN</strong></td>
+                        <td colspan="5">
+                            a. <strong>Obyek Garapan</strong>: Sistem Manajemen Sumber Daya Manusia<br>
+                            b. <strong>Bentuk Luaran</strong>: Kode program Java yang mengimplementasikan kelas Employee, Manager, Department, dan Project sesuai dengan studi kasus yang diberikan.
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><strong>INDIKATOR, KRITERIA DAN BOBOT PENILAIAN</strong></td>
+                        <td colspan="5">
+                            <ol>
+                                <li>Penerapan Konsep OOP (30%): Penilaian berdasarkan seberapa baik konsep pemrograman berorientasi objek diterapkan, termasuk penggunaan kelas, objek, metode, dan enkapsulasi.</li>
+                                <li>Sintaks Kode (30%): Penilaian akan fokus pada pendekatan yang benar untuk menangani error dan kesalahan,</li>
+                                <li>Struktur dan Kualitas Kode (20%): Kode harus ringkas dan efisien, struktur kode yang baik akan mudah dibaca, dengan penggunaan komentar yang relevan.</li>
+                                <li>Pengujian dan Keandalan (10%): Kode harus diuji secara menyeluruh dengan menggunakan skenario untuk memastikan keandalan.</li>
+                                <li>Dokumentasi (10%): Penyediaan dokumentasi.</li>
+                            </ol>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><strong>JADWAL PELAKSANAAN</strong></td>
+                        <td colspan="5">Tugas diberikan sebelum UTS</td>
+                    </tr>
+                    <tr>
+                        <td><strong>LAIN-LAIN</strong></td>
+                        <td colspan="5"></td>
+                    </tr>
+                    <tr>
+                        <td><strong>DAFTAR RUJUKAN</strong></td>
+                        <td colspan="5"></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
